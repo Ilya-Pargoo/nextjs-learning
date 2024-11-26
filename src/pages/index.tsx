0bb5679756704687ad50887cff380c7f98073158
IliaDigiWay <ilya.zabrotski@pargoo.com>
2024-11-26 14:29:01 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,4 +2,13 @@ function Home() {
   return <div></div>;
 }
 
+export const getServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: '/top-stories',
+      permanent: false,
+    },
+  };
+};
+
 export default Home;
