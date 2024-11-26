80408d121b8365e6cdfdf040d7c06de6297a8ee3
IliaDigiWay <ilya.zabrotski@pargoo.com>
2024-11-26 15:19:52 +0100

    Update formatDate.ts

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,7 +1,24 @@
-// import { format, parseISO } from 'date-fns';
-
 export const formatDate = (dateString: string): string => {
-  return '';
-  // const date = parseISO(dateString);
-  // return format(date, 'MMMM d, yyyy, HH:mm');
+  const date = new Date(dateString);
+
+  if (isNaN(date.getTime())) {
+    console.error('Invalid date string provided:', dateString);
+    return 'Invalid date';
+  }
+
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  const formattedDate = date.toLocaleDateString('en-US', options);
+
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  };
+  const formattedTime = date.toLocaleTimeString('en-US', timeOptions);
+
+  return `${formattedDate}, ${formattedTime}`;
 };
