7c0c58723afc9763f69c3bd43028d97eda723b86
IliaDigiWay <ilya.zabrotski@pargoo.com>
2024-11-26 15:26:20 +0100

    Update formatDate.ts

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,24 +1,25 @@
 export const formatDate = (dateString: string): string => {
-  const date = new Date(dateString);
+  // const date = new Date(dateString);
 
-  if (isNaN(date.getTime())) {
-    console.error('Invalid date string provided:', dateString);
-    return 'Invalid date';
-  }
+  // if (isNaN(date.getTime())) {
+  //   console.error('Invalid date string provided:', dateString);
+  //   return 'Invalid date';
+  // }
 
-  const options: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  };
-  const formattedDate = date.toLocaleDateString('en-US', options);
+  // const options: Intl.DateTimeFormatOptions = {
+  //   year: 'numeric',
+  //   month: 'long',
+  //   day: 'numeric',
+  // };
+  // const formattedDate = date.toLocaleDateString('en-US', options);
 
-  const timeOptions: Intl.DateTimeFormatOptions = {
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: false,
-  };
-  const formattedTime = date.toLocaleTimeString('en-US', timeOptions);
+  // const timeOptions: Intl.DateTimeFormatOptions = {
+  //   hour: '2-digit',
+  //   minute: '2-digit',
+  //   hour12: false,
+  // };
+  // const formattedTime = date.toLocaleTimeString('en-US', timeOptions);
 
-  return `${formattedDate}, ${formattedTime}`;
+  // return `${formattedDate}, ${formattedTime}`;
+  return dateString;
 };
