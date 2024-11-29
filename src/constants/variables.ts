12619363972e892fc90da84758ee78450dc0c31e
IliaDigiWay <ilya.zabrotski@pargoo.com>
2024-11-29 06:38:47 +0100

    Fixed missed STRAPI_API_TOKEN

--- src/constants/variables.ts
+++ src/constants/variables.ts
@@ -3,4 +3,5 @@ export const TOP_STORIES_API_URL =
 export const TOP_STORIES_API_KEY =
   process.env.NEXT_PUBLIC_TOP_STORIES_API_KEY ?? '';
 export const STRAPI_API_URL = process.env.STRAPI_API_URL ?? '';
+export const STRAPI_API_TOKEN = process.env.STRAPI_API_TOKEN ?? '';
 export const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY ?? '';
